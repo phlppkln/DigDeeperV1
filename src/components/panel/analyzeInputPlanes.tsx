a119f49e224f26e05d42eb62902ca928dc3d22bd
Philipp <klein.philipp@posteo.at>
2024-07-22 10:56:09 +0200

    add components analyzeInputPlanes and createInputPlanes

---
+++ src/components/panel/analyzeInputPlanes.tsx
@@ -0,0 +1,19 @@
+import { useEffect } from "react";
+
+
+
+const analyzeInputPlanes: React.FC = () => {
+
+    useEffect(() => {}, []);
+
+    //TODO: add sample sticky notes
+    
+    return (
+    
+    <div>
+
+    </div>
+);
+};
+
+export default analyzeInputPlanes;
---
+++ src/components/panel/createInputPlanes.tsx
@@ -0,0 +1,19 @@
+import { useEffect } from "react";
+
+
+
+const createInputPlanes: React.FC = () => {
+
+    useEffect(() => {}, []);
+
+    //TODO: add sample sticky notes
+    
+    return (
+    
+    <div>
+
+    </div>
+);
+};
+
+export default createInputPlanes;
