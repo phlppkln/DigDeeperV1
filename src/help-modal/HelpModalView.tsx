dca65fd5537821907a4b46fa959373872de8aaae
Philipp Klein <klein.philipp@posteo.at>
2024-09-16 14:36:43 +0200

    add about to helpModalView

--- src/help-modal/HelpModalView.tsx
+++ src/help-modal/HelpModalView.tsx
@@ -2,7 +2,6 @@ import { useEffect } from "react";
 import { createRoot } from "react-dom/client";
 import template from "../assets/images/template.png";
 
-
 const HelpModalView = () => {
   useEffect(() => {}, []);
 
@@ -15,53 +14,55 @@ const HelpModalView = () => {
       <div className="description-modal-content">
         <h1>DigDeeper</h1>
         <p>
-        DigDeeper lets you use the power of the infinite canvas to gather
-            peoples perspective through spatial positioning of sticky notes. Participants are 
-            able to express theirnp thoughts and ideas on a two-axis plane that allows them
-            to use spatial relationships of sticky notes. Furthermore, Dig Deeper allows you to 
-            visualize and analyze the spatial properties of these sticky notes to uncover key insights,
-            patterns, and trends.
+          DigDeeper lets you use the power of the infinite canvas to gather
+          peoples perspective through spatial positioning of sticky notes.
+          Participants are able to express theirnp thoughts and ideas on a
+          two-axis plane that allows them to use spatial relationships of sticky
+          notes. Furthermore, Dig Deeper allows you to visualize and analyze the
+          spatial properties of these sticky notes to uncover key insights,
+          patterns, and trends.
         </p>
         <h2>How does it work?</h2>
-        <p>We encourage you simply try it out, there is an option in the application.</p>
+        <p>
+          We encourage you simply try it out, there is an option in the
+          application.
+        </p>
         <p>
           <strong>
-            The most important things summed up for people who do not like instructions:
-            </strong>
-            </p>
-            <strong>
-            <ul>
-              <li>
-                Each question is represented by a frame and belongs to exactly
-                one participant.
-              </li>
-              <li>
-                You can create as many participants and questions as you want.
-              </li>
-              <li>Each response is represented by a note.</li>
-              <li>
-                Only sticky notes and their placement on the plane will be
-                included in the analysis.
-              </li>
-            </ul>
+            The most important things summed up for people who do not like
+            instructions:
           </strong>
+        </p>
+        <strong>
+          <ul>
+            <li>
+              Each question is represented by a frame and belongs to exactly one
+              participant.
+            </li>
+            <li>
+              You can create as many participants and questions as you want.
+            </li>
+            <li>Each response is represented by a note.</li>
+            <li>
+              Only sticky notes and their placement on the plane will be
+              included in the analysis.
+            </li>
+          </ul>
+        </strong>
         <h3>Input Plane</h3>
         <p>
           Each plane is represented by a frame. The title of the frame
-          identifies the participant and the topic of the plane. It must include a
-          participant identifier, followed by a colon and the topic
+          identifies the participant and the topic of the plane. It must include
+          a participant identifier, followed by a colon and the topic
           identifier. The title must be in the format{" "}
           <em>&lt;PersonId&gt;: &lt;QuestionId&gt;</em>
         </p>
         <p>
-          You can create a template frame with DigDeeper as well, to make things clearer. However, the most basic frame
-          looks like this:
+          You can create a template frame with DigDeeper as well, to make things
+          clearer. However, the most basic frame looks like this:
         </p>
         <div className="center-content">
-          <img
-            className="image-description"
-            src={template}
-          />
+          <img className="image-description" src={template} />
         </div>
         <p>
           It is possible to add other Miro items to the frame, such as
@@ -79,13 +80,15 @@ const HelpModalView = () => {
           create a frame for each question and participant.
         </p>
         <p>
-          As the questioning begins, you may want to introduce the format to the participants.
-          They should only use sticky notes to add an answer to the frames question. 
-          Then they place the notes on the plane, to further express themselves along the two axis.
+          As the questioning begins, you may want to introduce the format to the
+          participants. They should only use sticky notes to add an answer to
+          the frames question. Then they place the notes on the plane, to
+          further express themselves along the two axis.
         </p>
         <p>
-          After the questioning of all participants is done, you can start the analysis of the notes. You can explore the 
-          perspective of participants directly in DigDeeper or export the data as a JSON file.
+          After the questioning of all participants is done, you can start the
+          analysis of the notes. You can explore the perspective of participants
+          directly in DigDeeper or export the data as a JSON file.
         </p>
         <h2>Motivation</h2>
         <p>
@@ -104,12 +107,12 @@ const HelpModalView = () => {
         </p>
         <p>
           DigDeeper offers the ability to visualize and analyze spatial
-          relationships on the plane, facilitating a deeper understanding of the most important
-          areas. This creates a dynamic and immersive way to explore
-          different perspectives and helps you to identify key insights,
+          relationships on the plane, facilitating a deeper understanding of the
+          most important areas. This creates a dynamic and immersive way to
+          explore different perspectives and helps you to identify key insights,
           patterns, and trends. You can use DigDeeper to explore and quickly
           pinpoint the most crucial aspects of your discussions, interviews, or
-          brainstorming sessions. Furthermore, DigDeeper enables you to export 
+          brainstorming sessions. Furthermore, DigDeeper enables you to export
           the data, to explore it in your favorite tool as well.
         </p>
         <button
@@ -119,6 +122,38 @@ const HelpModalView = () => {
         >
           Close Instructions
         </button>
+      </div>
+      <div className="about-container">
+        <h2>About</h2>
+        This application was built as part of a master thesis for the study program "Media and Human-Centered Computing" at the TU Wien. 
+        <h3>Credits</h3>
+        <p>Thanks to the following projects, libraries, and resources:</p>
+        <ul>
+          <li>Ideas and Inspiration:</li>
+          <ul>
+            <li><a href="https://gamestorming.com/">Gamestorming</a></li>
+          </ul>
+          <li>Tools:</li>
+          <ul>
+          <li><a href="https://developers.miro.com/" target="_blank">Miro Developer Platform</a></li>
+          <li><a href="https://d3js.org" target="_blank">D3</a></li></ul>
+          <li>Styles, Icons, and Illustrations:</li>
+          <ul>
+            <li>
+              <a href="https://www.mirotone.xyz/" target="_blank">
+                Mirotone
+              </a>
+            </li>
+            <li>
+            Cooperation, puzzle Icon from  <a
+                href="https://icon-icons.com/users/jdQwB54y56M5KbbF8AzQF/icon-sets/"
+                target="_blank"
+              >
+                Posse Studio
+              </a>
+            </li>
+          </ul>
+        </ul>
       </div>
     </div>
   );
